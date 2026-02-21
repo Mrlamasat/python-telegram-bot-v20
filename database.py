aba5802189a0f9d8a4051e05b4cb8c61bc6dfe59
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 04:30:19 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,10 @@ import sqlite3
 
 def init_db():
     conn = sqlite3.connect("bot.db")
-    cursor = conn.cursor()
+    cur = conn.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS videos (
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS videos(
         video_id TEXT PRIMARY KEY,
         file_id TEXT,
         poster_id TEXT,
@@ -16,8 +16,8 @@ def init_db():
     )
     """)
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS likes (
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS likes(
         video_id TEXT,
         user_id INTEGER,
         UNIQUE(video_id, user_id)
@@ -27,11 +27,11 @@ def init_db():
     conn.commit()
     conn.close()
 
-def execute(query, params=(), fetch=False):
+def db(query, params=(), fetch=False):
     conn = sqlite3.connect("bot.db")
-    cursor = conn.cursor()
-    cursor.execute(query, params)
+    cur = conn.cursor()
+    cur.execute(query, params)
     conn.commit()
-    result = cursor.fetchall() if fetch else None
+    data = cur.fetchall() if fetch else None
     conn.close()
-    return result
+    return data
