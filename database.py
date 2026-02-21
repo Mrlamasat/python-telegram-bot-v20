bf14107629b17f2cc0cdf729f06bc217154df510
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 05:33:15 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,27 +1,30 @@
 import sqlite3
 import os
 
-DB_PATH = "/app/data/videos.db"
+DB_PATH = "data/videos.db"
 
 def init_db():
-    # تأكد من وجود المجلد
-    os.makedirs("/app/data", exist_ok=True)
-    
+    os.makedirs("data", exist_ok=True)  # تأكد من وجود مجلد البيانات
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-
-    # إنشاء جدول videos إذا لم يكن موجوداً
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS videos (
-            video_id TEXT PRIMARY KEY,
-            file_id TEXT,
-            poster_id TEXT,
-            title TEXT,
-            episode INTEGER,
-            quality TEXT,
-            duration TEXT
-        )
+    CREATE TABLE IF NOT EXISTS videos (
+        video_id TEXT PRIMARY KEY,
+        file_id TEXT,
+        poster_id TEXT,
+        title TEXT,
+        episode INTEGER,
+        quality TEXT,
+        duration TEXT
+    )
     """)
     conn.commit()
     conn.close()
     print(f"[INFO] Database initialized at {DB_PATH}")
+
+def execute(query, params=()):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    conn.commit()
+    conn.close()
