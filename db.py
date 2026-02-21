efedfcbf5dc22113856c554e68e3828a3bf4e1b4
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 07:43:09 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,28 +1,31 @@
 import sqlite3
 
-DB_NAME = "bot_data.db"
+DB_PATH = "bot_data.db"
 
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS videos (
             v_id TEXT PRIMARY KEY,
-            poster_id TEXT NOT NULL,
-            title TEXT,
-            ep_num INTEGER NOT NULL,
-            quality TEXT NOT NULL,
-            status TEXT NOT NULL
+            duration TEXT,
+            poster_id TEXT,
+            poster_caption TEXT,
+            status TEXT,
+            ep_num INTEGER,
+            quality TEXT
         )
     ''')
     conn.commit()
     conn.close()
 
 def db_execute(query, params=(), fetch=True):
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute(query, params)
     conn.commit()
     res = cursor.fetchall() if fetch else None
     conn.close()
     return res
+
+init_db()
