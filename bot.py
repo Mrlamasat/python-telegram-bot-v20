da6c22e02f63e142805a8bcdac066b02d37f698c
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 06:32:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,29 @@
-from telegram.ext import ApplicationBuilder
-import config, asyncio
+# bot.py
+import asyncio
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ConversationHandler
 from database import init_db
-from handlers import admin, public, user
+from handlers import admin
 
 async def main():
     await init_db()
-    app = ApplicationBuilder().token(config.BOT_TOKEN).build()
-    # إعداد القنوات
-    app.bot_data["PUBLIC_CHANNEL"] = config.PUBLIC_CHANNEL
+    app = ApplicationBuilder().token("8579897728:AAHCeFONuRJca-Y1iwq9bV7OK8RQotldzr0").build()
 
-    # تسجيل الهاندلرز
-    admin.register_handlers(app)
-    public.register_handlers(app)
-    user.register_handlers(app)
+    # Conversation Handler لإضافة الحلقات
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('add', admin.start_add)],
+        states={
+            admin.TITLE: [MessageHandler(filters.VIDEO, admin.receive_video)],
+            admin.POSTER: [MessageHandler(filters.PHOTO, admin.receive_poster)],
+            admin.EPISODE_NUM: [MessageHandler(filters.TEXT & ~filters.COMMAND, admin.receive_title)],
+            admin.QUALITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, admin.receive_episode_number)],
+            admin.CONFIRM: [CallbackQueryHandler(admin.receive_quality)]
+        },
+        fallbacks=[]
+    )
 
+    app.add_handler(conv_handler)
+
+    print("البوت يعمل الآن ✅")
     await app.run_polling()
 
 asyncio.run(main())
