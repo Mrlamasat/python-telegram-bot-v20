5a8f41874e3865598c8e03b9c4bb7c4c201221eb
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 07:42:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import os
 
-# إعدادات API و البوت
-API_ID = int(os.environ.get("API_ID",35405228))
+# ================= إعدادات API و البوت =================
+# إذا لم توجد متغيرات البيئة، سيتم استخدام القيم التالية مؤقتًا
+API_ID = int(os.environ.get("API_ID", 35405228))
 API_HASH = os.environ.get("API_HASH", "dacba460d875d963bbd4462c5eb554d6")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8579897728:AAHCeFONuRJca-Y1iwq9bV7OK8RQotldzr0")
 
-# القنوات
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1003547072209))          # القناة الخاصة بإضافة الحلقات
-PUBLIC_CHANNEL = os.environ.get("PUBLIC_CHANNEL", "RamadanSeries26")      # القناة العامة للنشر
+# ================= القنوات =================
+# قناة رفع الحلقات الخاصة (يجب أن يكون البوت مشرف فيها)
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1003547072209))
 
-# يوزر البوت الجديد (لتحويل الحلقات القديمة)
-NEW_BOT_USERNAME = os.environ.get("NEW_BOT_USERNAME", "@Bottemo_bot")
+# القناة العامة للنشر (يجب أن يكون البوت مشرف فيها)
+PUBLIC_CHANNEL = os.environ.get("PUBLIC_CHANNEL", "RamadanSeries26")
+
+# ================= البوت الجديد لتحويل الحلقات القديمة =================
+# بدون @
+NEW_BOT_USERNAME = os.environ.get("NEW_BOT_USERNAME", "Bottemo_bot")
