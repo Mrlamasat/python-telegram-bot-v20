8ff3463077c342ee6d16d88667976f3ba3227a93
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 06:30:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_TOKEN = "توكن_البوت_هنا"
-ADMIN_CHANNEL = -1001234567890  # قناة المشرفين
-PUBLIC_CHANNEL = -1009876543210 # القناة العامة
+BOT_TOKEN = "8579897728:AAHCeFONuRJca-Y1iwq9bV7OK8RQotldzr0"
+ADMIN_CHANNEL = -7720165591  # قناة المشرفين
+PUBLIC_CHANNEL = -RamadanSeries26 # القناة العامة
 DATABASE_PATH = "data/videos.db"
 MIN_VIDEO_DURATION = 10  # ثواني
