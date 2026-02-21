47e7b687334453e83968f152c0d9a3a198c6308c
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 06:21:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,6 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# توكن البوت
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8579897728:AAHCeFONuRJca-Y1iwq9bV7OK8RQotldzr0")
-
-# قناة التخزين الخاصة بالمشرفين
-STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID", -1003547072209))
-
-# القناة العامة للنشر
-PUBLIC_CHANNEL_ID = os.getenv("PUBLIC_CHANNEL_ID", "RamadanSeries26")
-
-# قناة الاشتراك الإجباري للمستخدمين
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "MoAlmohsen")
-
-# معرفات المشرفين (يمكنك إضافة أكثر من معرف مفصول بفاصلة)
-ADMINS = list(map(int, os.getenv("ADMINS", "7720165591").split(",")))
-
-# مسار قاعدة البيانات (Railway يستخدم /data/ للمساحات الدائمة)
-DB_PATH = os.getenv("DB_PATH", "/data/bot_data.db")
+# config.py
+BOT_TOKEN = "توكن البوت هنا"
+ADMIN_CHANNEL = -1001234567890  # قناة المشرفين
+PUBLIC_CHANNEL = -1009876543210 # القناة العامة
+DATABASE_PATH = "data/videos.db"
+MIN_VIDEO_DURATION = 10  # ثواني
