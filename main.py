c6bf91779f65cef65b266dce158c1ad3fb593ea3
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 07:37:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-from pyrogram import Client, filters
-from config import API_ID, API_HASH, BOT_TOKEN, CHANNEL_ID
-import handlers, commands
+from pyrogram import Client
+from db import init_db
+from handlers import register_handlers
+from commands import register_commands
+from config import API_ID, API_HASH, BOT_TOKEN
 
-app = Client("BottemoBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+init_db()
 
-# استقبال الفيديوهات
-app.add_handler(filters.chat(CHANNEL_ID) & (filters.video | filters.document), handlers.handle_video)
-# استقبال البوستر
-app.add_handler(filters.chat(CHANNEL_ID) & filters.photo, handlers.handle_poster)
-# استقبال رقم الحلقة
-app.add_handler(filters.chat(CHANNEL_ID) & filters.text & ~filters.command(["start"]), handlers.handle_ep_number)
-# أوامر المستخدم
-app.add_handler(filters.command("start") & filters.private, commands.start_handler)
-# الضغط على زر الحلقات
-app.add_handler(filters.callback_query(filters.regex(r"^watch_")), commands.watch_callback)
+app = Client("TeleBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-print("✅ البوت يعمل الآن...")
+register_handlers(app)
+register_commands(app)
+
+print("✅ البوت جاهز للعمل...")
 app.run()
