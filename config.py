641ad2129771cb1209a48a795ec1663b1d3440be
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 04:43:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID"))
-PUBLIC_CHANNEL_ID = int(os.getenv("PUBLIC_CHANNEL_ID"))
-
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")  # مثال: @MyChannel
-
+PUBLIC_CHANNEL_ID = os.getenv("PUBLIC_CHANNEL_ID")
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
 ADMINS = list(map(int, os.getenv("ADMINS").split(",")))
+DB_PATH = os.getenv("DB_PATH", "bot_data.db")
