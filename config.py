092bbea30fbfbf7538bae95d3ee3695606b442f0
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 04:22:13 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID"))  # قناة التخزين
+PUBLIC_CHANNEL_ID = int(os.getenv("PUBLIC_CHANNEL_ID"))    # قناة النشر
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")         # يوزر قناة الاشتراك الإجباري
+
+ADMINS = list(map(int, os.getenv("ADMINS").split(",")))
