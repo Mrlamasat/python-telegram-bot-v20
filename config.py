675bc44fb49f5c45c2b92202e0a777baa2650a22
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 06:23:02 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-BOT_TOKEN = "توكن البوت هنا"
+BOT_TOKEN = "توكن_البوت_هنا"
 ADMIN_CHANNEL = -1001234567890  # قناة المشرفين
 PUBLIC_CHANNEL = -1009876543210 # القناة العامة
 DATABASE_PATH = "data/videos.db"
