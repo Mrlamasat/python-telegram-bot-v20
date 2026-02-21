b0b1f4c666db279da1e237eaecaf71a150367d0e
Mrlamasat <mr.lamasat@gmail.com>
2026-02-21 04:30:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID"))  # قناة التخزين
-PUBLIC_CHANNEL_ID = int(os.getenv("PUBLIC_CHANNEL_ID"))    # قناة النشر
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")         # يوزر قناة الاشتراك الإجباري
+STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID"))
+PUBLIC_CHANNEL_ID = int(os.getenv("PUBLIC_CHANNEL_ID"))
+
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")  # مثال: @MyChannel
 
 ADMINS = list(map(int, os.getenv("ADMINS").split(",")))
